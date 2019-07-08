7c161892ee811f0f3cf5202115ce0b3ee968dbbc
mrudhularaya <mrudhula119876@gmail.com>
2019-07-08 20:50:57 +0530

    Initial Commit

---
+++ pig.py
@@ -0,0 +1,25 @@
+#get sentance from user
+original = input("Enter your sentance: ").strip().lower()
+#split sent into words
+words=original.split()
+#loop words and conv to pig latin
+new_words=[]
+for word in words:
+    if word[0] in "aeiou":
+        new_word=word+"yay"
+        new_words.append(new_word)
+    else:
+        vowel_pos=0
+        for letter in word:
+            if letter not in "aeiou":
+                vowel_pos=vowel_pos+1
+            else:
+                break
+        cons = word[:vowel_pos:]
+        the_rest=word[vowel_pos:]
+        new_word=the_rest + cons + "ay"
+        new_words.append(new_word)
+#stick words back together
+output=" ".join(new_words)
+#output the final string
+print(output)
